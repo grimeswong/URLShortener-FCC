e0b828b43aa028263d7eb3b8678dc1900ebb71b2
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-18 05:42:58 +1000

    Implement response post data with its url parameter

--- server.js
+++ server.js
@@ -3,7 +3,9 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 require('dotenv').config();
+
 
 var cors = require('cors');
 
@@ -23,6 +25,9 @@ app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -32,8 +37,9 @@ app.get('/', function(req, res){
 
 
 // your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.route('/api/shorturl/new').post(function(req, res) {
+  console.log(req.body.url)
+  res.json({original_url: req.body.url})
 });
 
 
