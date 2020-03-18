ca5c6369d4b40a8680cfa6e0095abcdbd1a3b76f
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-18 19:19:35 +1000

    Fix the issue of request url that can't pass the dns lookup

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var dns = require('dns');
+var sha1 = require('sha1')
 require('dotenv').config();
 
 
@@ -21,6 +22,14 @@ mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology
     console.log("Database is connected successfully!");
   })
   .catch(error => console.error(`Cannot connect to the database due to ${error}`));
+
+  var Schema = mongoose.Schema;
+  var urlSchema = new Schema({  // Create Schema
+    _id: {type: String, required: true},
+    url: {type: String, required: true},
+    short_url: {type: String, required: true}
+  })
+  var URL = mongoose.model("URL", urlSchema); // Create Model
 
 app.use(cors());
 
@@ -39,18 +48,38 @@ app.get('/', function(req, res){
 
 // your first API endpoint...
 app.route('/api/shorturl/new').post(function(req, res) {
-  console.log(req.body.url)
-  var regex = /https?:\/\/|( |w{3}\.)/g;  // replace the protocol and www prefix
-  var sortedURL = req.body.url.replace(regex, '').trim(); // trim the start and end white space
-  console.log(sortedURL);
-  console.log(typeof(sortedURL));
+  var trimURL = req.body.url.trim(); // trim the start and end white space or new lines
+  var regex = /https?:\/\/(w{3}\.)?/g;  // replace the protocol and www prefix
+  var sortedURL = trimURL.replace(regex, '');
+  console.log(JSON.stringify(sortedURL));
   dns.lookup(sortedURL, function(err, address, family) { // need to take out the 'https://' or 'http://'
     console.log(`error = ${err}`);  // error = null
     console.log(`address = ${address}, family = ${family}`)
     if(err === null) {
       // Response with a shorturl
-      //store data in database with a hash ID
-      // res.json({original_url: req.body.url})
+
+      // var saveURL = new URL({
+      //   _id: sha1(trimURL),
+      //   url: trimURL,
+      //   short_url: sha1(trimURL)
+      // })
+      //
+      // URL.find({url: trimURL}, function(err, data) {
+      //   if(err) {return console.error(err)}
+      //   console.log(`data length ${data}`);
+      // })
+      //
+      // saveURL.save(function(err, data) {
+      //   console.log(data);
+      //   res.json({
+      //     original_url: data.url,
+      //     short_url: data.short_url
+      //   })
+      // })
+
+      // Define Schema and setup Model
+      // store data in database with a hash ID
+      res.json({original_url: req.body.url})
     } else {
       // Response with a invalid url
       res.json({error: "invalid URL"});
